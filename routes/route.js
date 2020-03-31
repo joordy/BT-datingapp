edf89240b0c3a5a2f9a4fb1da24cbb7691117bc1
joordy <jmfronik@gmail.com>
2020-03-31 23:47:36 +0200

    inserted liking feature, match page shows up and matches are displayed on matchlist.ejs

--- routes/route.js
+++ routes/route.js
@@ -6,7 +6,6 @@ require('dotenv').config();
 // Database calling
 let db = null;
 let usersCollection = null;
-
 let url = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@${process.env.DB_URL}${process.env.DB_END}`;
 
 mongo.MongoClient.connect(url, { useUnifiedTopology: true }, function(
@@ -33,12 +32,22 @@ router.post('/match', match); // Jordy
 router.get('/matchlist', matchList); // Jordy
 router.get('/filter', filter); // Veerle
 router.post('/', postFilter); // Veerle
-
 router.get('/*', error); // Veerle
 
+function deleteYourself(remove_u) {
+    // To remove yourself from match page
+    let yourSelf = usersCollection.find({ _id: '5e70aa4227f0bb83c16adf21' });
+    let index = remove_u.findIndex(p => p.id === yourSelf);
+    completeCollection = remove_u;
+    return completeCollection;
+}
+
+// Routing functions
 async function home(req, res, next) {
+    // Routes function home, graps every user with 'seen: false' and shows them on page.
     try {
-        res.render('index.ejs');
+        let allUsers = await usersCollection.find({ seen: false }).toArray();
+        res.render('index.ejs', { users: allUsers });
     } catch (err) {
         console.log(err);
     }
@@ -83,16 +92,32 @@ async function showUser(req, res, next) {
 }
 
 async function match(req, res, next) {
+    // res.render('match.ejs');
     try {
-        res.render('match.ejs');
+        let users = await usersCollection.find({ seen: false }).toArray();
+        let matchedUser = deleteYourself(users);
+        let x = completeCollection.length - 1;
+
+        if (req.body.like) {
+            usersCollection.updateOne({ _id: completeCollection[x]._id }, { $set: { match: true, seen: true } });
+            console.log(
+                `you have a like with ${completeCollection[x].firstName}, and the ID is ${completeCollection[x]._id}`
+            );
+            res.render('match.ejs', { users: matchedUser }); // data uit database halen en printen onder noemer 'users' in EJS templates
+        } else if (req.body.dislike) {
+            usersCollection.updateOne({ _id: completeCollection[x]._id }, { $set: { match: false, seen: true } });
+            res.redirect('/');
+        }
     } catch (err) {
         console.log(err);
     }
 }
 
 async function matchList(req, res, next) {
+    // Route match overview, graps every user with 'match: true' and will be displayed on overview page.
     try {
-        res.render('matchlist.ejs');
+        let matches = await usersCollection.find({ match: true }).toArray();
+        res.render('matchlist.ejs', { users: matches });
     } catch (err) {
         console.log(err);
     }
@@ -115,6 +140,7 @@ async function postFilter(req, res, next) {
 }
 
 async function error(req, res, next) {
+    // Veerle of Jordy
     try {
         res.render('404.ejs');
     } catch (err) {
@@ -123,15 +149,3 @@ async function error(req, res, next) {
 }
 
 module.exports = router;
-
-// seen
-// firstName
-// lastName
-// DateOfBirth
-// gender
-// photo
-// movies
-// prefGender
-// prefMovie
-// likesYou
-// match
